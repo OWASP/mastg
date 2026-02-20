088662d84bdff9be59d8369c1c5b1590db0fc355
mudabbir <shazan.mudabbir34@gmail.com>
2026-02-20 11:34:42 +0530

    Fix

--- demos/android/MASVS-CODE/MASTG-DEMO-AXXX/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-semgrep -c ../../../../rules/mastg-android-overlay-protection.yml MastgTest_reversed.java > output.txt
--- demos/android/MASVS-CODE/MASTG-DEMO-XXXX/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-semgrep -c ../../../../rules/mastg-android-overlay-attack.yml activity_vulnerable_login.xml > output.txt
